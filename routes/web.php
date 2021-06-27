5de4a886c5fa68573f2f3397bd07ed30b675cad3
bcarlu <briancardona87@gmail.com>
2021-06-26 19:11:34 -0500

    Se agregan rutas para visualizar, crear y eliminar compañias y empleados

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,49 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+//Route to show admin options and data resume
+// Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+// Route to show list of companies
+Route::get('/dashboard/companys', [CompanysController::class, 'index']);
+
+// Route to render creation company form
+Route::get('/dashboard/companys/create', [CompanysController::class, 'create']);
+
+// Route to store new company in database
+Route::post('/dashboard/companys/store', [CompanysController::class, 'store']);
+
+// Route to render confirm delete company page
+Route::get('/dashboard/companys/delete/{id}', function(){
+    return view('confirmCompanyDelete');
+});
+
+// Route to delete company from database
+Route::post('/dashboard/companys/delete/{id}', [CompanysController::class, 'destroy']);
+
+// Route to show list of companies
+Route::get('/dashboard/employees', [EmployeesController::class, 'index']);
+
+// Route to render creation employee form
+Route::get('/dashboard/employees/create', [EmployeesController::class, 'create']);
+
+// Route to store new employee in database
+Route::post('/dashboard/employees/store', [EmployeesController::class, 'store']);
+
+// Route to render confirm delete employee page
+Route::get('/dashboard/employees/delete/{id}', function(){
+    return view('confirmEmployeeDelete');
+});
+
+// Route to delete employee from database
+Route::post('/dashboard/employees/delete/{id}', [EmployeesController::class, 'destroy']);
 
 require __DIR__.'/auth.php';
