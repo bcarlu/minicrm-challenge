26b05095bc215df0b891fe60ce293516cc9c5013
bcarlu <briancardona87@gmail.com>
2021-07-02 20:50:07 -0500

    Se agregaformulario de busqueda

--- resources/views/companys.blade.php
+++ resources/views/companys.blade.php
@@ -1,11 +1,14 @@
 <x-app-layout>
     <x-slot name="slot">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pt-5">
-            <label for="search">Buscar</label>
-            <input type="text" id="search" name="search">
+            <!-- Search form -->
+            <form action="/dashboard/companys/search">
+                <input type="text" id="search" name="search" placeholder="Buscar por nombre">
+                <button type="submit" class="mt-2 p-2 rounded bg-green-500 text-white">Buscar</button>
+            </form>
             <div class="flex">
                 <div class="text-4xl p-2">
-                    <h1>Companys</h1>
+                    <h1>Compañias</h1>
                 </div> 
             </div>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-2">
